a7a72ea02fe71978f5f0691bbccd4770ac486ec7
kynoprueba <kynfestivo@gmail.com>
2025-08-07 20:08:37 -0500

    codigo funcionanando sin cargar datos

--- sketch.js
+++ sketch.js
@@ -1,54 +1,13 @@
-/*let numeros=[
-    {dur:2},
-    {dur:10},
-    {dur:32},
-    {dur:15},
-    {dur:44},
-     {dur:100}
-    
-]
-     */
-/*let misdatos=["primero","segundo","tercero","cuarto","quinto","noveno"];*/
+
+d3.json("datos.json").then(function(datos){
+   let n=datos;
+   
+
+
+});
+
 
 let misdatos2=[1,2,3,4,5,6,7,8,9,11,10];
-
-let miArray = [
-  { nombre: "Juan", edad: 3 },
-  { nombre: "Maria", edad: 5 },
-  { nombre: "Pedro", edad: 4 }
-];
-
-for(i=0; i< miArray.length; i++ ){
-    console.log(i);
-}
-
-
-let obj1=miArray[0].edad;
-let num= Number(obj1)
-console.log(num);
-
-
-/*let misdatos=[
-    {
-    nombre:"dato 1",
-    cal:1
-    },
-    {
-        nombre:"dato 2",
-        cal:5
-    },
-    {
-        nombre:"dato 3",
-        cal:3
-    },
-    {
-        nombre:"dato 4",
-        cal:5
-    }
-
-];
-
-*/
 
 let misvg =d3.select("#uno")
 .attr("width",800)
@@ -62,7 +21,7 @@ misvg.selectAll("circle")
 .append("circle")
 .attr("cx",(d,i)=>i*100)
 .attr("cy",100)
-.attr("r",(d,i)=>d*num)
+.attr("r",(d,i)=>d*6)
 
 /*
 let misvg =d3.selectAll("p")
